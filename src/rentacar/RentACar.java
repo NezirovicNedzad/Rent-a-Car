b31cf95bc76c4eb1c1eb10e257e4d60bd58a6ec5
nezirovicnedzad <nedzad@200010@gmail.com>
2026-06-25 03:55:03 +0200

    Complete

---
+++ src/rentacar/RentACar.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rentacar;
+
+/**
+ *
+ * @author Administrator
+ */
+import login.LoginPage;
+
+public class RentACar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LoginPage loginObj = new LoginPage();
+        loginObj.setVisible(true);
+    }
+    
+}
---
+++ src/session/Session.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package session;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Session {
+    
+    public static String ime;
+    public static String prezime;
+    public static String email;
+}
